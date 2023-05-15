d5b1c41775911dafb180ddf54e11939e6559fc00
Alfred4c <109122219+Alfred4c@users.noreply.github.com>
2023-05-15 17:26:59 +0800

    Update trello.py

--- trello.py
+++ trello.py
@@ -1,12 +1,17 @@
-import requests, os
+import requests, os, sys
 from urllib.parse import unquote
 import argparse
+ATTACHMENT_REQUEST_TIMEOUT = 30  # 30 seconds
+
 # Read the API keys from the environment variables
 key = os.getenv('TRELLO_API_KEY', '')
 token = os.getenv('TRELLO_TOKEN', '')
+
 # key = r'7298aecff5a0da0a0bd40d1665ec56e9'
 # token = r'ATTAcac28ff8d501cd25bd88a028295fd5603b1e409802a78ae33fb582a08d8544ceE45A3437'
 # card_id = 'fm5ttZRn'
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--card_id',  help='id of card')
 args = parser.parse_args()
@@ -50,7 +55,15 @@ for attachment in attachments:
     attachment_url = attachment["url"]
     filename = decode_if_url_encoded(attachment_url.split("/")[-1])
 
-    response = requests.get(attachment_url)
+    try:
+        response = requests.get(attachment_url,
+                               stream = True,
+                               timeout = ATTACHMENT_REQUEST_TIMEOUT,
+                               headers = {"Authorization":"OAuth oauth_consumer_key=\{}\",oauth_token=\"{}\"".format(key,token)})
+    except Exception:
+        sys.stderr.write('Failed download: {}'.format(filename))
+        continue
+        
     with open(filename, "wb") as f:
         f.write(response.content)
 
