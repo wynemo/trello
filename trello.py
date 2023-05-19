e941f0a828e2e444d1d01746bd097160368c710e
Tommy Green <tommygreen@Tommys-Huawei-MateBook-14s.local>
2023-05-19 11:13:25 +0800

    refactor:add cad args

--- trello.py
+++ trello.py
@@ -1,34 +1,32 @@
 import argparse
 import os
 import sys
-from urllib.parse import unquote
+from urllib.parse import unquote, urlparse
 
 import requests
 
 ATTACHMENT_REQUEST_TIMEOUT = 30  # 30 seconds
 
 # Read the API keys from the environment variables
-key = os.getenv('TRELLO_API_KEY', '')
-token = os.getenv('TRELLO_TOKEN', '')
-
-# key = r'7298aecff5a0da0a0bd40d1665ec56e9'
-# token = r'ATTAcac28ff8d501cd25bd88a028295fd5603b1e409802a78ae33fb582a08d8544ceE45A3437'
-# card_id = 'fm5ttZRn'
-
+key = os.getenv("TRELLO_API_KEY", "")
+token = os.getenv("TRELLO_TOKEN", "")
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--card_id',  help='id of card')
+parser.add_argument("--card", "-c", help="card")
 args = parser.parse_args()
-card_id = args.card_id
-url = f"https://api.trello.com/1/cards/{card_id}" + "?attachments=true&attachment_fields=url"
+card: str = args.card
+if card.startswith("http"):
+    card_id = urlparse(card).path.lstrip("/").split("/")[1]
+else:
+    card_id = card
+print(f"card_id is {card_id}")
+url = (
+    f"https://api.trello.com/1/cards/{card_id}"
+    + "?attachments=true&attachment_fields=url"
+)
 
-params = {
-    "key": key,
-    "token": token
-}
-headers = {
-    "Accept": "application/json"
-}
+params = {"key": key, "token": token}
+headers = {"Accept": "application/json"}
 
 
 def decode_if_url_encoded(string):
@@ -43,29 +41,35 @@ response = requests.get(url, headers=headers, params=params)
 card_data = response.json()
 
 md_content = card_data["desc"]
-card_name = card_data['name']
+card_name = card_data["name"]
 
-attachments = card_data['attachments']
+attachments = card_data["attachments"]
 
 for attachment in attachments:
     attachment_url = attachment["url"]
     attachment_id = attachment["id"]
     filename = decode_if_url_encoded(attachment_url.split("/")[-1])
     folder_name = f"attachments_{card_id}/{attachment_id}"
-    os.makedirs(folder_name, exist_ok=True) 
+    os.makedirs(folder_name, exist_ok=True)
 
     try:
-        response = requests.get(attachment_url,
-                               stream = True,
-                               timeout = ATTACHMENT_REQUEST_TIMEOUT,
-                               headers = {"Authorization":"OAuth oauth_consumer_key=\"{}\",oauth_token=\"{}\"".format(key,token)})
+        response = requests.get(
+            attachment_url,
+            stream=True,
+            timeout=ATTACHMENT_REQUEST_TIMEOUT,
+            headers={
+                "Authorization": 'OAuth oauth_consumer_key="{}",oauth_token="{}"'.format(
+                    key, token
+                )
+            },
+        )
     except Exception:
-        sys.stderr.write('Failed download: {}'.format(filename))
+        sys.stderr.write("Failed download: {}".format(filename))
         continue
 
     filepath = os.path.join(folder_name, filename)
     md_content = md_content.replace(attachment_url, filepath)
-        
+
     with open(filepath, "wb") as f:
         f.write(response.content)
 
